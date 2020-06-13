03077c608a40831d96dd40e4d4dcee682a857d55
campbellde <dcam.kkpsi@gmail.com>
2020-06-14 00:31:15 +0200

    Add files via upload

--- js/slideshow.js
+++ js/slideshow.js
@@ -5,14 +5,18 @@ var time = 3000;
 
 //Image List
 images[0] = 'img/HVE_Meat1.jpg';
-images[1] = 'img/HVE_Meat2.jpg';
-images[2] = 'img/HVE_Meat3.jpg';
-images[3] = 'img/HVE_Meat4.jpg';
-images[4] = 'img/HVE_Bull.jpg';
-images[5] = 'img/HVE_group.jpg';
-images[6] = 'img/sheep.jpg';
-images[7] = 'img/sheep2.jpg';
-images[7] = 'img/Hochland2.jpg';
+images[1] = 'img/HVE_Meat4.jpg';
+images[2] = 'img/Steaks.jpg';
+images[3] = 'img/T-bone.jpg';
+images[4] = 'img/Tomahawk.jpg';
+images[5] = 'img/Flansteak.jpg';
+images[6] = 'img/Land.jpg';
+images[7] = 'img/HVE_Bull.jpg';
+images[8] = 'img/HVE_group.jpg';
+images[9] = 'img/sheep.jpg';
+images[10] = 'img/sheep2.jpg';
+images[11] = 'img/Hochland2.jpg';
+images[12] = 'img/Hochland.jpg';
 
 //Change Image
 function changeImg() {
