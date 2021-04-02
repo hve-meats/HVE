fad98d0f73de2ca34879ef6516f7b5dad4e85f5c
hve-meats <80672645+hve-meats@users.noreply.github.com>
2021-04-02 18:23:36 +0200

    Add files via upload

--- js/slideshow.js
+++ js/slideshow.js
@@ -5,19 +5,22 @@ var time = 3000;
 
 //Image List
 images[0] = 'img/HVE_Meat1.jpg';
-images[1] = 'img/HVE_Meat4.jpg';
-images[2] = 'img/Steaks.jpg';
-images[3] = 'img/T-bone.jpg';
-images[4] = 'img/Tomahawk.jpg';
-images[5] = 'img/Flanksteak.jpg';
-images[6] = 'img/Land.jpg';
-images[7] = 'img/HVE_Bull.jpg';
-images[8] = 'img/HVE_group.jpg';
-images[9] = 'img/sheep.jpg';
-images[10] = 'img/sheep2.jpg';
-images[11] = 'img/Hochland2.jpg';
-images[12] = 'img/Hochland.jpg';
-images[13] = 'img/group2.jpg';
+images[1] = 'img/Butchering.jpg';
+images[2] = 'img/HVE_Meat4.jpg';
+images[3] = 'img/Steaks.jpg';
+images[4] = 'img/T-bone.jpg';
+images[5] = 'img/Tomahawk.jpg';
+images[6] = 'img/Flanksteak.jpg';
+images[7] = 'img/Land.jpg';
+images[8] = 'img/HVE_Bull.jpg';
+images[9] = 'img/HVE_group.jpg';
+images[10] = 'img/sheep.jpg';
+images[11] = 'img/sheep2.jpg';
+images[12] = 'img/Hochland2.jpg';
+images[13] = 'img/Hochland.jpg';
+images[14] = 'img/group2.jpg';
+images[15] = 'img/BullResting.jpg';
+
 
 //Change Image
 function changeImg() {
